a05cb298a768d21d18c80472e37da37e918c4d7d
henry9982 <128062028+henry9982@users.noreply.github.com>
2023-11-17 00:30:02 +0630

    sec commit after adding up signinwithemailandpassword feature

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -1,21 +1,34 @@
 import { useState } from 'react'
 import {auth ,googleProvider } from '../config/firebase.js'
-import {createUserWithEmailAndPassword ,signOut ,signInWithPopup} from 'firebase/auth'
+import {createUserWithEmailAndPassword ,signOut ,signInWithPopup, signInWithEmailAndPassword} from 'firebase/auth'
 
 const Auth = () => {
     const [email,setEmail] = useState("")
     const [password,setPassword] = useState("")
 
+    const [loginEmail,setLoginEmail] = useState("")
+    const [loginPassword,setLoginPassword] = useState("")
+
     console.log(auth?.currentUser?.email);
     console.log(auth?.currentUser?.photoURL);
 
-    const singIn = async()=>{
+    const signup = async()=>{
       try{
         const data = await createUserWithEmailAndPassword(auth,email,password)
       }catch(err){
         console.error(err);
       }
     }
+
+    const login = async ()=>{
+      try{
+        const data = await signInWithEmailAndPassword(auth,loginEmail,loginPassword)
+      }catch(err){
+        console.error(err);
+      }
+    }
+
+
     const singInWithGoogle = async()=>{
         const data = await signInWithPopup(auth,googleProvider)
       
@@ -29,11 +42,27 @@ const Auth = () => {
     }
   return (
     <div>
+        <small>Create new account</small>
+        <br />
         <input value={email} onChange={(e)=>setEmail(e.target.value)} type="text" placeholder='Email...' />
         <input value={password} onChange={(e)=>setPassword(e.target.value)} type="text" placeholder='Password...' />
-        <button onClick={singIn}>Sign in</button>
+        <button onClick={signup}>Sign up</button>
+        <br />
+        <br />
         <button onClick={singInWithGoogle}>Sign in with google</button>
+        <br />
+        <br />
+        <small>login if you already have an account</small>
+        <br />
+        <input value={loginEmail} onChange={(e)=>setLoginEmail(e.target.value)} type="text" placeholder='Email...' />
+        <input value={loginPassword} onChange={(e)=>setLoginPassword(e.target.value)} type="text" placeholder='Password...' />
+        <button onClick={login}>login</button>
+        <br />
+        <br />
         <button onClick={logOut}>Log out</button>
+        <br />
+        <br />
+        
     </div>
   )
 }
